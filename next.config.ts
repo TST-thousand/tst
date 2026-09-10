8b15ea3eaeceadf0423b5415ea532c1ceaef9246
Amgaa <gansukhamgalanbaatarr13@gmail.com>
2026-09-10 12:12:30 +0800

    limit next build workers

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   devIndicators: false,
   output: "standalone",
+
+  experimental: {
+    cpus: 1,
+  },
 };
 
 export default nextConfig;
